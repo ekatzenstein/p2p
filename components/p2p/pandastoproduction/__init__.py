7565dd5276b80f1bc7ed6e84c1be4e298bfe2022
clintonmonk <clintonmonk@users.noreply.github.com>
2019-01-23 12:16:34 -0500

    Initial p2p library and build.

---
+++ components/p2p/pandastoproduction/__init__.py
@@ -0,0 +1,5 @@
+
+def publish(stuff):
+    print("Publishing...")
+    print(stuff)
+    print("Done!")
---
+++ components/p2p/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+
+def parse_requirements(fname):
+    """Loads requirements from a pip requirements file with fname"""
+    with open(os.path.join(os.path.dirname(__file__), fname)) as fhandle:
+        reader = fhandle.readlines()
+    return [line for line in reader if line and not line.startswith("#")]
+
+
+setup(
+    name="pandastoproduction",
+    version="0.1.0",
+    description="Pandas to Production",
+    url="https://git.enigma/hackweek/pandas-to-production/p2p",
+    author="Enigma Technologies",
+    packages=find_packages(),
+    install_requires=parse_requirements("requirements.txt"),
+    zip_safe=False,
+)
