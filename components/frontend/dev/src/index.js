ed65ff8972773be04c8e9ed549972734627aa841
Erick Katzenstein <erickkatzenstein@ip-10-22-169-200.ec2.internal>
2019-01-23 18:07:37 -0500

    downgrade to react 15

--- components/frontend/dev/src/index.js
+++ components/frontend/dev/src/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { connectRouter } from 'connected-react-router'
+import { connectRouter } from 'connected-react-router';
 import createHistory from 'history/createBrowserHistory';
 import App from './components';
 import * as serviceWorker from './serviceWorker';
@@ -9,36 +9,31 @@ import { Route, Router } from 'react-router-dom';
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { routerMiddleware, ConnectedRouter } from 'connected-react-router'
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
 
-import './style.css'
-
+import './style.css';
 
 const history = createHistory();
 const historyMiddleware = routerMiddleware(history);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
 export const store = createStore(
-  rootReducer(history), {},
-  composeEnhancers(
-    applyMiddleware(
-      thunk, historyMiddleware
-    )
-  )
+  rootReducer(history),
+  {},
+  composeEnhancers(applyMiddleware(thunk, historyMiddleware))
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Fragment>
+      <div>
         <Route path={`/`} component={App} />
         <Route exact path={`/risks/:riskId`} component={App} />
-      </Fragment>
+      </div>
     </ConnectedRouter>
-  </Provider>
-  , document.getElementById('root'));
-
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls. Learn
