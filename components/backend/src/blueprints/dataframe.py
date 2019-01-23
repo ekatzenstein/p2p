a3336ed17157ea5f305110cf2eda61dacc418e10
Blaine Boman <blaine.boman@gmail.com>
2019-01-23 12:17:43 -0500

    Initial dataframe upload to minio

--- components/backend/src/blueprints/dataframe.py
+++ components/backend/src/blueprints/dataframe.py
@@ -1,7 +1,9 @@
 """Flask view functions for the dataframe resource endpoint."""
-from flask import (Blueprint, jsonify)
+import os
 
+from flask import (Blueprint, request, current_app)
 from flask_apispec import use_kwargs, marshal_with, doc
+from minio import Minio
 
 from models import db, Dataframe
 from .schemas import DataframeSchema
@@ -30,6 +32,42 @@ def create_dataframe(digest, url):
 @marshal_with(DataframeSchema(many=False), 200)
 def get_dataframe(dataframe_id):
     return Dataframe.query.get(dataframe_id)
+
+
+def _allowed_file(filename):
+    return True
+
+
+def _secure_filename(filename, dataframe_id):
+    return f'dataframe_{dataframe_id}.csv'
+
+
+@DATAFRAME.route('/<int:dataframe_id>', methods=['POST'])
+def create_dataframe_content(dataframe_id):
+    dataframe = Dataframe.query.get(dataframe_id)
+    if not dataframe:
+        return 'No dataframe found', 400
+
+    file = request.files.get('file', None)
+    if file and _allowed_file(file.filename):
+        filename = _secure_filename(file.filename, dataframe_id)
+        full_filepath = os.path.join(
+            current_app.config['UPLOAD_DIRECTORY'], filename)
+        file.save(full_filepath)
+        # TODO: move minioClient somewhere global
+        minioClient = Minio(current_app.config['MINIO_ENDPOINT'],
+                            access_key=current_app.config['MINIO_ACCESS_KEY'],
+                            secret_key=current_app.config['MINIO_SECRET_KEY'],
+                            secure=False)
+        etag = minioClient.fput_object('dataframes', f'dataframe_{dataframe_id}.csv', full_filepath)
+        os.remove(full_filepath)
+
+        dataframe.url = f"http://{current_app.config['MINIO_ENDPOINT']}/dataframes/{filename}"
+        db.session.commit()
+
+        return '', 204, {'content-length': 0}
+
+    return 'No file found', 400
 
 
 @DATAFRAME.route('/<int:dataframe_id>', methods=['DELETE'])
